8d73eba31d5f9412b6531ad557102e862e5d7aea
abdelmoughit555 <fouhamtrance@gmail.com>
2020-01-15 20:50:47 +0000

    remove image

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -19,7 +19,7 @@ class CategoryResource extends JsonResource
             'name' => $this->name,
             'slug' => $this->slug,
             'description' => $this->description,
-            'image' => $this->image["url"]
+            'image' => ""
         ];
     }
 }
